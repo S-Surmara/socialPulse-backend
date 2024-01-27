58921cb372448cb899d5ccdd27a30e5d3f569488
Subham Surmara <anjalisurmara@Subhams-MacBook-Air.local>
2024-01-27 13:30:11 +0530

    first commit

---
+++ src/main/java/com/socialPulse/socialPulse/SocialPulseApplication.java
@@ -0,0 +1,13 @@
+package com.socialPulse.socialPulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialPulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialPulseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/socialPulse/socialPulse/SocialPulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socialPulse.socialPulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialPulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
